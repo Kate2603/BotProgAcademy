1112c4a45f40182758291bcbb7daeba721c32d78
Kateryna Velychko <107401289+Kate2603@users.noreply.github.com>
2024-02-16 23:00:53 +0200

    added distance calculator

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println( "Hello world!" );
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.println("What is the speed of your car?");
+        var speed = scanner.nextDouble();
+
+        while (true) {
+            System.out.println( "How long are you traveling?" );
+            var time = scanner.nextDouble();
+
+            if(time == 0)
+                break;
+
+            var distance = speed * time;
+
+            System.out.println( "You traveled the distance during this time: " + distance );
+        }
     }
 }
